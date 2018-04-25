8b74e7b7d267ae4091db79f3d2a0e2991c1b5f4a
Your Name <you@example.com>
2018-04-25 13:37:29 -0300

    timezoom

--- src/admin/app/components/admin/directives/dmec-tracking.directive.js
+++ src/admin/app/components/admin/directives/dmec-tracking.directive.js
@@ -35,6 +35,33 @@
 			scope.getAllReadingSince = getAllReadingSince;
 			scope.onEndAtChange = onEndAtChange;
 			scope.showDrillingMecChart = true;
+			scope.actionButtonUseOperationStartDate = actionButtonUseOperationStartDate;
+
+			var inputRangeForm;
+
+			try{
+				inputRangeForm = JSON.parse(localStorage.getItem('dmecTrackingInputRangeForm'));
+			}catch(e){
+				inputRangeForm = null;
+			}
+
+			if(!inputRangeForm){
+				inputRangeForm = {
+					realtime: true,
+					last: 1,
+					toMilliseconds: '3600000',
+				}
+			}
+
+			scope.inputRangeForm = inputRangeForm;
+
+			function actionButtonUseOperationStartDate(startDate){
+				if(scope.inputRangeForm.useOperationStartDate){
+					scope.inputRangeForm.startTime = new Date(startDate);
+					scope.inputRangeForm.startTime.setMilliseconds(0);
+					scope.inputRangeForm.startTime.setSeconds(0);
+				}
+			}
 
 			/**
 			 * Quando 'showDrillingMecChart' é true a diretiva aparece
