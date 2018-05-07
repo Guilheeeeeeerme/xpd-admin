da76db5f264ec0bf6ed5af90f488c678ecdcbdd7
Gezzy Ramos <gramos@rzxtecnologia.com.br>
2018-05-07 12:29:23 -0300

    agora as consultas são feitas sempre de 12 em 12 horas

--- src/admin/app/components/admin/directives/dmec-tracking.directive.js
+++ src/admin/app/components/admin/directives/dmec-tracking.directive.js
@@ -135,23 +135,33 @@
 					startTime = operationStartDate;
 				}
 
-
 				var loopLimit = new Date();
 				var loopStartTime = new Date(startTime);
 				var loopEndTime = new Date(startTime);
+
+				loopStartTime.setMinutes(0);
+				loopStartTime.setSeconds(0);
+
+				if (loopStartTime.getHours() < 12) {
+					loopStartTime.setHours(0);
+				} else {
+					loopStartTime.setHours(12);
+				}
 
 				var promiseList = [];
 
 				while (loopEndTime.getTime() < loopLimit.getTime()) {
 
 					loopEndTime.setHours(loopStartTime.getHours() + 12);
+					var loopEndTimestamp = loopEndTime.getTime();
 
 					if (loopEndTime.getTime() > loopLimit.getTime()) {
-						loopEndTime = loopLimit;
+						// loopEndTimestamp = loopLimit.getTime();
+						loopEndTimestamp = null;
 					}
 
 					promiseList.push($q(function (resolve, reject) {
-						readingSetupAPIService.getAllReadingByStartEndTime(loopStartTime.getTime(), loopEndTime.getTime(), resolve, reject);
+						readingSetupAPIService.getAllReadingByStartEndTime(loopStartTime.getTime(), loopEndTimestamp, resolve, reject);
 					}));
 
 					loopStartTime = new Date(loopEndTime);
