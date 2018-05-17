e6df2b6f7c92282fd855c9e7c16333cbae278158
Gezzy Ramos <gramos@rzxtecnologia.com.br>
2018-05-17 12:18:45 -0300

    teste de category finalizado

--- test/setup-api/category-setupapi/category-setupapi.test.js
+++ test/setup-api/category-setupapi/category-setupapi.test.js
@@ -5,15 +5,94 @@
 
 	function categorySetupTestController($scope, categorySetupAPIService) {
 
-		$scope.dados = {
+		$scope.data = {};
+	
+		// TESTE OK
+		// categorySetupAPIService.getCategoryName(1,
+		// 	function (result) {
+		// 		$scope.data.getCategoryName = {
+		// 			success: result
+		// 		};
+		// 	},
+		// 	function (error) {
+		// 		$scope.data.getCategoryName = {
+		// 			error: error
+		// 		};
+		// 	}
+		// );
 
-		};
-		
-		categorySetupAPIService.getCategoryName(id, (result) => {
-			
-		}) {
+		// TESTE OK
+		// categorySetupAPIService.getList(
+		// 	function (result) {
+		// 		$scope.data.getList = {
+		// 			success: result
+		// 		};
+		// 	},
+		// 	function (error) {
+		// 		$scope.data.getList = {
+		// 			error: error
+		// 		};
+		// 	}
+		// );
 
-		}
-		console.log(categorySetupAPIService);
+		// TESTE OK
+		// var category = {
+		// 	initial: 'C01',
+		// 	name: 'category 1',
+		// 	parentId: 1,
+		// };
+		// categorySetupAPIService.insertObject(category,
+		// 	function (result) {
+		// 		$scope.data.getList = {
+		// 			success: result
+		// 		};
+		// 	},
+		// 	function (error) {
+		// 		$scope.data.getList = {
+		// 			error: error
+		// 		};
+		// 	}
+		// );
+
+		// TESTE OK
+		// var category = {
+		// 	id: 2,
+		// 	initial: 'C02',
+		// 	name: 'category 2',
+		// 	parentId: 1,
+		// };
+		// categorySetupAPIService.removeObject(category,
+		// 	function (result) {
+		// 		$scope.data.getList = {
+		// 			success: result
+		// 		};
+		// 	},
+		// 	function (error) {
+		// 		$scope.data.getList = {
+		// 			error: error
+		// 		};
+		// 	}
+		// );
+
+		// TESTE OK
+		// var category = {
+		// 	id: 2,
+		// 	initial: 'C01',
+		// 	name: 'category 1',
+		// 	parentId: 1,
+		// };
+		// categorySetupAPIService.updateObject(category,
+		// 	function (result) {
+		// 		$scope.data.getList = {
+		// 			success: result
+		// 		};
+		// 	},
+		// 	function (error) {
+		// 		$scope.data.getList = {
+		// 			error: error
+		// 		};
+		// 	}
+		// );
+
 	}
 })();
