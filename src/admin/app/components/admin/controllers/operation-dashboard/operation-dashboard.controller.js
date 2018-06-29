039664e09b640635fe9de3c06aeeb6736ae5123c
Gezzy Ramos <gramos@rzxtecnologia.com.br>
2018-06-29 12:44:01 -0300

    Consistency Information agora mostra info da última trip, conn e joint

--- src/admin/app/components/admin/controllers/operation-dashboard/operation-dashboard.controller.js
+++ src/admin/app/components/admin/controllers/operation-dashboard/operation-dashboard.controller.js
@@ -14,6 +14,7 @@
 		var selectedEventType;
 
 		$scope.statusPanel = [];
+		$scope.jointInfo = {};
 		$scope.dados = {
 			connectionEvents: [],
 			tripEvents: [],
@@ -48,7 +49,7 @@
 
 				actionButtonBuildForecast(selectedBaseLine, selectedEventType);
 				calcAccScore();
-				prepareFailure();
+				getLastTwoEvents($scope.operationData.eventContext);
 			} catch (error) {
 				// setTimeout(onReadyToStart, 5000);
 			}
@@ -183,8 +184,120 @@
 			$scope.accScore = $scope.operationData.shiftContext.accScore.totalScore / $scope.operationData.shiftContext.accScore.eventScoreQty;
 		}
 
+		function getLastTwoEvents(eventContext) {
+
+			var lastEvents = eventContext.lastEvents;
+			var lastTwoEvents = [];
+
+			var conn = false;
+			var trip = false;
+
+			for (var index = lastEvents.length-1; index >= 0; index--) {
+
+				var event = lastEvents[index];				
+				
+				if (event.eventType == 'CONN') {
+					lastTwoEvents['CONN'] = prepareLastEvent(event);
+					conn = true;
+				}
+
+				if (event.eventType == 'TRIP') {
+					lastTwoEvents['TRIP'] = prepareLastEvent(event);
+					trip = true;
+				}
+
+				if (conn && trip) break;
+
+			}
+
+			$scope.lastTwoEvents = lastTwoEvents;
+			$scope.jointInfo = prepareJoint();
+		}
+
+		function prepareLastEvent(event) {
+
+			//afterVpoor é só para aparecer a barra vermelha após o poor
+
+			var eventDuration = event.duration / 1000;
+			var displacement = 1;
+
+			if(event.eventType == 'TRIP') {
+				displacement = $scope.operationData.operationContext.currentOperation.slipsThreshold;
+			}
+
+			var vtargetTime = displacement / event.vtarget;
+			var voptimumTime = displacement / event.voptimum;
+			var vstandardTime = displacement / event.vstandard;
+			var vpoorTime = displacement / event.vpoor;
+			var afterVpoorTime = vpoorTime + (vpoorTime / 10);
+			var voptimumPercentage = calcPercentage(voptimumTime, afterVpoorTime);
+			var vstandardPercentage = calcPercentage(vstandardTime, afterVpoorTime) - voptimumPercentage;
+			var vpoorPercentage = calcPercentage(vpoorTime, afterVpoorTime) - (voptimumPercentage + vstandardPercentage);
+			var afterVpoorPercentage = 100 - (voptimumPercentage + vstandardPercentage + vpoorPercentage);
+			var colorPerformance = getColorPerformance(eventDuration, voptimumTime, vstandardTime, vpoorTime);
+
+			return {
+				duration: eventDuration,
+				vtargetTime: vtargetTime,
+				percentageDuration: calcPercentage(eventDuration, afterVpoorTime),
+				voptimumTime: voptimumTime,
+				vstandardTime: vstandardTime,
+				vpoorTime: vpoorTime,
+				afterVpoorTime: vpoorTime + (vpoorTime / 10),
+				colorPerformance: colorPerformance,
+
+				voptimumPercentage: voptimumPercentage,
+				vstandardPercentage: vstandardPercentage,
+				vpoorPercentage: vpoorPercentage,
+				afterVpoorPercentage: afterVpoorPercentage
+			};
+		}
+
+		function prepareJoint() {
+			var tripEvent = $scope.lastTwoEvents['TRIP'];
+			var connEvent = $scope.lastTwoEvents['CONN'];
+
+			console.log('trip', 'conn');
+
+			var duration = tripEvent.duration + connEvent.duration;
+			var voptimumTime = tripEvent.voptimumTime + connEvent.voptimumTime;
+			var vstandardTime = tripEvent.vstandardTime + connEvent.vstandardTime;
+			var vpoorTime = tripEvent.vpoorTime + connEvent.vpoorTime;
+			var afterVpoorTime = tripEvent.afterVpoorTime + connEvent.afterVpoorTime;
+			var percentageDuration = calcPercentage(duration, afterVpoorTime);
+			var colorPerformance = getColorPerformance(duration, voptimumTime, vstandardTime, vpoorTime);
+
+			return {
+				duration: duration,
+				voptimumTime: voptimumTime,
+				vstandardTime: vstandardTime,
+				vpoorTime: vpoorTime,
+				afterVpoorTime: afterVpoorTime,
+				percentageDuration: percentageDuration,
+				colorPerformance: colorPerformance,
+			}
+
+		}
+
+		function calcPercentage(partTime, totalTime) {
+			return (partTime * 100) / totalTime;
+		}
+
+		function getColorPerformance(duration, voptimumTime, vstandardTime, vpoorTime) {
+
+			if (duration < voptimumTime) {
+				return '';
+			} else if (duration <= vstandardTime ) {
+				return 'progress-bar-success';
+			} else if (duration <= vpoorTime) {
+				return 'progress-bar-warning';
+			} else {
+				return 'progress-bar-danger';
+			}
+		}
+
 		function getTotalFailureTime(startTime, endTime) {
-			if(!endTime) return 0;
+			if (!endTime) return 0;
 
 			var diffTime = new Date(endTime).getTime() - new Date(startTime).getTime();
 			return new Date(diffTime);
