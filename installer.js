43425e696c79b38bf52102aab1e6bb57b6ee6a58
Your Name <you@example.com>
2018-09-18 10:37:27 -0300

    electron also as 32

--- installer.js
+++ installer.js
@@ -1,15 +1,24 @@
 var electronInstaller = require('electron-winstaller');
 var path = require('path');
 
-var resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: path.join(__dirname, 'build', 'Admin-win32-x64'),
-    outputDirectory: path.join(__dirname, 'bin', 'Admin-win32-x64'),
-    title: 'XPD Admin',
-    description: 'System Setup and Admin Control',
-    authors: 'RZX Tecnologia',
-    setupExe: 'Admin-win32-x64.exe',
-    iconUrl: path.join(__dirname, 'dist', 'logo.ico'),
-    setupIcon: path.join(__dirname, 'dist', 'logo.ico'),
-});
+function buildFor (arch) {
 
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
+    console.log("Building ", arch);
+
+    var resultPromise = electronInstaller.createWindowsInstaller({
+        appDirectory: path.join(__dirname, 'build', 'Admin-win32-' + arch + ''),
+        outputDirectory: path.join(__dirname, 'bin', 'Admin-win32-' + arch + ''),
+        title: 'XPD Admin',
+        description: 'System Setup and Admin Control',
+        authors: 'RZX Tecnologia',
+        setupExe: 'Admin-win32-' + arch + '.exe',
+        iconUrl: path.join(__dirname, 'dist', 'logo.ico'),
+        setupIcon: path.join(__dirname, 'dist', 'logo.ico'),
+    });
+    
+    resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
+
+}
+
+buildFor('x64');
+// buildFor('ia32');
