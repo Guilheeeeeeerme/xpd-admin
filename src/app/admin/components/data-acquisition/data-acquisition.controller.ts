5272e146311740a83e8c0a5be6bd25e30e0d7835
roosevelt <roosevelt1@gmail.com>
2021-10-06 16:06:33 -0300

    minor adjustment in actionButtonConnect

--- src/app/admin/components/data-acquisition/data-acquisition.controller.ts
+++ src/app/admin/components/data-acquisition/data-acquisition.controller.ts
@@ -121,7 +121,7 @@ export class DataAcquisitionController {
 		});
 
 		function actionButtonConnect() {
-			$rootScope.serverInfo.serialPort = $rootScope.serverInfo.serialPort.trim();
+			$rootScope.serverInfo.serialPort = $rootScope.serverInfo.serialPort?.trim();
 			witsDataService.emitEv('user-request-connection', $rootScope.serverInfo);
 		}
 
